3a4e512b529312cffbc647eb15868461041f6c01
LostOne <denis2485545@gmail.com>
2023-09-20 12:37:33 +0300

    added js for vis and hide

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ const swiper = new Swiper('.swiper', {
     
   });
 
+
+  // document.querySelector('hide').onclick=function(){
+
+
+  //   let hide = document.querySelectorAll('hide').style.display;
+
+  //   if(hide.style.display === 'flex') {
+  //     style.display = 'none'
+  //   } else {
+  //     style.display = 'flex'
+  //   }
+  // };
+
+
+
+const initialButton = document.getElementById("initialButton");
+const returnButton = document.getElementById("returnButton");
+const hiddenBlocks = document.getElementById("hiddenBlocks");
+
+
+initialButton.addEventListener("click", function () {
+    initialButton.style.display = "none";
+    hiddenBlocks.style.display = "none";
+
+
+    returnButton.style.display = "flex";
+});
+
+returnButton.addEventListener("click", function () {
+
+    returnButton.style.display = "none";
+
+    initialButton.style.display = "flex";
+    hiddenBlocks.style.display = "flex";
+});
