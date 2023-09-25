420944dcaf1e4eafff8c6f341a3237d3c8ea1a11
LostOne <denis2485545@gmail.com>
2023-09-25 16:22:12 +0200

    work on js

--- js/script.js
+++ js/script.js
@@ -10,49 +10,74 @@ const swiper = new Swiper('.swiper', {
     
   });
 
+  const initialButton = document.getElementById("initialButton");
+  const returnButton = document.getElementById("returnButton");
+  const hiddenBlocks = document.querySelectorAll(".hiddenCart");
+  const serviceCards = document.querySelectorAll('.services-card');
+  const mediaQuery769 = window.matchMedia('(min-width: 768px)');
+  const mediaQuery1121 = window.matchMedia('(min-width: 1120px)');
 
-const initialButton = document.getElementById("initialButton");
-const returnButton = document.getElementById("returnButton");
+initialButton.onclick = function(el) {
 
-const hiddenBlocks = document.querySelectorAll(".hiddenCart");
+  if(mediaQuery769.matches) {
+      initialButton.style.display = 'none';
+      returnButton.style.display = 'flex';
+      hiddenBlocks.forEach(block => this.classList.toggle('hodden', true));
+      toggleCards(false);
+  } else {
+    initialButton.style.display = 'flex';
+    returnButton.style.display = 'none';
+    hiddenBlocks.forEach(block => this.classList.toggle('hodden', false));
+    toggleCards(true);
+  }
+}
 
-initialButton.addEventListener('click', function() {
+function toggleCards() {
+   
+    const displayValue = show ? 'flex' : 'none';
+}
 
-  initialButton.style.display = 'none'
-  returnButton.style.display = 'flex'
 
-  if (window.innerWidth <= 1120) {
-    for (let i = 0; i < 11; i++) {
-      if(i == 3 || i >= 7) {
-        document.querySelectorAll('.services__card')[i].style.display = 'none';
-      }
-    }
-  } 
-  else if (window.innerWidth > 1020) {
-    for (let i = 8; i <= 11; i++) {
-      document.querySelectorAll('.services__card')[i].style.display = 'none';
-    }
-  };
   
-});
-
-
-returnButton.addEventListener('click', function() {
-
-  initialButton.style.display = 'flex'
-  returnButton.style.display = 'none'
-
-  if (window.innerWidth <= 1120) {
-    for (let i = 0; i < 11; i++) {
-      if(i == 3 || i >= 7) {
-        document.querySelectorAll('.services__card')[i].style.display = 'flex';
-      }
-    }
-  } 
-  else if (window.innerWidth > 1020) {
-    for (let i = 8; i <= 11; i++) {
-      document.querySelectorAll('.services__card')[i].style.display = 'flex';
-    }
-  };
+  // // Создаем объекты MediaQueryList для двух медиазапросов
   
-});
+  // // Добавляем слушатели события change к объектам MediaQueryList
+  // mediaQuery769.addListener(handleMediaChange);
+  // mediaQuery1121.addListener(handleMediaChange);
+  
+  // // Инициализация медиазапросов при загрузке страницы
+  // handleMediaChange(mediaQuery769);
+  // handleMediaChange(mediaQuery1121);
+  
+  // function handleMediaChange(mediaQuery) {
+  //   if (mediaQuery.matches) {
+  //     // Медиазапрос срабатывает
+  //     initialButton.style.display = 'none';
+  //     returnButton.style.display = 'flex';
+  //     hiddenBlocks.forEach(block => block.classList.toggle('hidden', true));
+  //     toggleServiceCards(false);
+  //   } else {
+  //     // Медиазапрос не срабатывает
+  //     initialButton.style.display = 'flex';
+  //     returnButton.style.display = 'none';
+  //     hiddenBlocks.forEach(block => block.classList.toggle('hidden', false));
+  //     toggleServiceCards(true);
+  //   }
+  // }
+  
+  // function toggleServiceCards(show) {
+  //   const displayValue = show ? 'flex' : 'none';
+  
+  //   if (window.innerWidth <= 1120) {
+  //     for (let i = 0; i < 11; i++) {
+  //       if (i == 3 || i >= 7) {
+  //         serviceCards[i].style.display = displayValue;
+  //       }
+  //     }
+  //   } else if (window.innerWidth > 1020) {
+  //     for (let i = 8; i <= 11; i++) {
+  //       serviceCards[i].style.display = displayValue;
+  //     }
+  //   }
+  // }
+  
